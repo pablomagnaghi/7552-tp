551abc5e7e14e7b655b4ca92eb0fdc5659155a41
enzoguagnini <enzoguagnini@gmail.com>
2012-04-02 17:40:08 +0000

    initial

---
+++ Tp Taller 2/src/modelo/Ide.cpp
@@ -0,0 +1,17 @@
+/*
+ * Ide.cpp
+ *
+ *  Created on: 02/04/2012
+ *      Author: metal
+ */
+
+#include "Ide.h"
+
+Ide::Ide() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Ide::~Ide() {
+	// TODO Auto-generated destructor stub
+}
