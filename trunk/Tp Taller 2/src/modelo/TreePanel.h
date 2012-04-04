0ecfd8340b19e17cfbcb3ad0ae23ce92d12f4f94
enzoguagnini@gmail.com <enzoguagnini@gmail.com@c7024519-3262-ab14-e071-a6ddd6ec04fd>
2012-04-04 03:51:15 +0000

    Avances con treeview

--- trunk/Tp Taller 2/src/modelo/TreePanel.h
+++ trunk/Tp Taller 2/src/modelo/TreePanel.h
@@ -10,27 +10,31 @@
 
 #include "Proyecto.h"
 #include <iostream>
+#include "../Constantes.h"
 using namespace std;
 
 class Proyecto;
 
-class TreePanel : Gtk::TreeView{
+typedef GtkRequisition Requisition;
+
+class TreePanel{
 private:
 	//Tree model columns:
-		  class ModeloColumnas : public Gtk::TreeModel::ColumnRecord
-		  {
-		  public:
+	class ModeloColumnas: public Gtk::TreeModel::ColumnRecord {
+	public:
 
-		    ModeloColumnas()
-		    { add(m_col_Nombre);}
+		ModeloColumnas() {
+			add(m_col_Nombre);
+		}
 
-		    Gtk::TreeModelColumn<string> m_col_Nombre;
-		  };
+		Gtk::TreeModelColumn<string> m_col_Nombre;
+	};
 
-		  ModeloColumnas m_Columnas;
-		  Glib::RefPtr<Gtk::TreeStore> refTreeModel;
-		  Proyecto* proyecto;
-
+	ModeloColumnas m_Columnas;
+	Glib::RefPtr<Gtk::TreeStore> refTreeModel;
+	Gtk::TreeView treeView;
+	Proyecto* proyecto;
+	void enlazarWidgets();
 public:
 	TreePanel(Proyecto* proy);
 	virtual ~TreePanel();
