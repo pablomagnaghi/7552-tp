4c6cba0619d642a25020e3480a3474690255ddf5
luisalbarracin <luisalbarracin@c7024519-3262-ab14-e071-a6ddd6ec04fd>
2012-12-17 01:43:08 +0000

    Métricas del modelo.

--- TpTaller2V2/src/modelo/validacion/MetricasVisitor.h
+++ TpTaller2V2/src/modelo/validacion/MetricasVisitor.h
@@ -15,22 +15,40 @@ class MetricasVisitor: public ModeloVisitor {
 private:
 
 	int cantidadDiagramas;
+
+	// Una posición del mapa por diagrama
 	std::map<std::string, int> cantidadComponentes;
 	std::map<std::string, int> cantidadEntidadesNuevas;
 	std::map<std::string, int> cantidadEntidadesGlobales;
 	std::map<std::string, int> cantidadRelaciones;
 	std::map<std::string, int> cantidadJerarquias;
+	std::map<std::string, int> cantidadIdentificadores;
+	std::map<std::string, int> cantidadAtributosEntidadesNuevas;
+	std::map<std::string, int> cantidadIdentificadoresEntidadesNuevas;
+	std::map<std::string, int> cantidadAtributosRelaciones;
+	std::map<std::string, int> cantidadAtributosIdentificadores;
 
 	void inicializar(Proyecto*);
 	void inicializar(Diagrama*);
 	void imprimirMetricasProyecto();
 	void imprimirMetricasDiagrama(Diagrama*);
 
+	int getCantidadComponentesTotal();
+	int getCantidadEntidadesNuevasTotal();
+	int getCantidadEntidadesGlobalesTotal();
+	int getCantidadRelacionesTotal();
+	int getCantidadJerarquiasTotal();
+	int getCantidadIdentificadoresTotal();
+
 	float calcularComponentesPromedio();
 	float calcularEntidadesNuevasPromedio();
 	float calcularEntidadesGlobalesPromedio();
 	float calcularRelacionesPromedio();
 	float calcularJerarquiasPromedio();
+	float calcularAtributosEntidadesNuevasPromedio();
+	float calcularIdentificadoresEntidadesNuevasPromedio();
+	float calcularAtributosRelacionesPromedio();
+	float calcularAtributosIdentificadoresPromedio();
 
 public:
 
